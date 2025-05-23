228fbb90e3d9625f2133121ff01b3e2a22b91722
Jitpanoo Polawath <jitpanoopolawath@gmail.com>
2025-05-23 18:41:36 +0700

    Added struct instead for light and material. Change naming

--- Architect's View/fragment.fs
+++ Architect's View/fragment.fs
@@ -1,12 +1,28 @@
 #version 330 core
 out vec4 FragColor;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+uniform Material material;
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform Light light; 
+
 in vec3 Normal; 
 in vec3 FragPos; 
-uniform vec3 objectColor;
+
 uniform int ID;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 viewPos;
+
 void main()
 {
 	if(ID == 1){
@@ -14,24 +30,22 @@ void main()
 	}else{
 
 		// Ambient
-		float ambientStrength = 0.1;
-		vec3 ambient = ambientStrength * lightColor;
+		vec3 ambient = light.ambient * material.ambient;
 
 		// Diffuse
 		vec3 norm = normalize(Normal);
-		vec3 lightDir = normalize(lightPos - FragPos); 
+		vec3 lightDir = normalize(light.position - FragPos); 
 		float diff = max(dot(norm, lightDir), 0.0);
-		vec3 diffuse = diff * lightColor;
+		vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
 		// Specular
-		float specularStrength = 0.5;
 		vec3 viewDir = normalize(viewPos - FragPos);
 		vec3 halfwayDir = normalize(lightDir + viewDir);
-		float spec = pow(max(dot(norm, halfwayDir), 0.0), 100);
-		vec3 specular = specularStrength * spec * lightColor; 
+		float spec = pow(max(dot(norm, halfwayDir), 0.0), material.shininess);
+		vec3 specular = light.specular * (spec * material.specular);  
 
 		// Combined
-		vec3 result = (ambient + diffuse + specular) * objectColor;
+		vec3 result = ambient + diffuse + specular;
 		FragColor = vec4(result, 1.0);
 	}
 };
